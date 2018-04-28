a80530cc09e5f10233f76544fad8bc1e48debf75
zhangyuhua <zyh2@nuoee.com>
2018-04-28 11:03:49 +0800

    优化我的广告

--- web/js/myad.js
+++ web/js/myad.js
@@ -1,4 +1,15 @@
 $(function(){
+  if(window.location.href.indexOf("?") > -1){
+    var arr = window.location.href.split("?"), objs = arr[1].split("&"), obj = {};
+    $.each(objs,function(index,item){
+      obj[item.split("=")[0]] = item.split("=")[1]
+    })
+    $(".item_list").removeClass("item_list_active")
+    .eq(obj.type === "ONLINEBUY"?0:1).addClass("item_list_active")
+    $(".content_title_list").removeClass("content_title_list_active")
+      .eq(obj.status === "OPENING"?0:1).addClass("content_title_list_active")
+    $(".content_list").hide().eq(obj.status === "OPENING"?0:1).show()
+  }
   
   $(".content_title_list").on("click",function(){
       $(this).toggleClass("content_title_list_active").
@@ -13,9 +24,4 @@ $(function(){
       $(".content").hide()
       $("."+$(this).attr("data-tar")).show()
   })
-
-
-
-
-
 })

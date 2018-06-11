b7c55ed20248da8e3de9899383dc1aa1544cb47a
zhangyuhua <zyh2@nuoee.com>
2018-06-11 16:17:11 +0800

    增加移动资源

---
+++ web/mobile/js/common.js
@@ -0,0 +1,129 @@
+/**
+ * @name send the vcode
+ * @param config
+ * @constructor
+ */
+window.GETCODE = function (config) {
+
+  var regCell = /^\d{8,}$/, // 手机正则
+    // 邮箱正则
+    regEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    // 页面地址
+    addr = $("form").attr("action"),
+    address = (addr.indexOf("reg") > -1) ? addr : '/forgot',
+    // 点击的按钮
+    button = config.btn,
+    // 数据来源
+    input = config.input;
+  input.on("focus", function () {
+    $(this).parent().next("span.error_text").remove()
+  });
+  button.on("tap", function (e) {
+    e.preventDefault();
+    if ($(this).attr("data-sending") === '1') return;
+    if (!input.val()) return;
+
+    if (config.email) {
+      if (!regEmail.test(input.val())) {
+        console.log("email error")
+        return false;
+      }
+    } else {
+      if (!regCell.test(input.val())) {
+        console.log("cell error")
+        return false;
+      }
+    }
+    var datas = {}
+    if (config.email) {
+      datas.email = input.val();
+    } else {
+      datas.cell = input.val();
+      datas.countriesId = config.country.val();
+    }
+    $.ajax({
+      method: "post",
+      url: config.api + address,
+      data: datas,
+      success: function (data, status, xhr) {
+        if (data.code === "10000") {
+          button.attr("data-sending", '1').addClass("code_sending");
+          var time = 120;
+          var renderText = setInterval(function () {
+            if (time) {
+              button.text(--time + "s后再次获取")
+            } else {
+              window.clearInterval(renderText);
+              button.attr("data-sending", '0').text("发送验证码").removeClass("code_sending")
+            }
+          }, 1000)
+        }
+        if (data.code === "99998") {
+          var notice, isReg = (address === "/register");
+          notice = '<span class="error_text"></span>';
+          var dom = $(notice);
+          dom.text('手机号' + (isReg ? '已' : '未') + '注册');
+          input.parent().after(dom);
+        }
+      },
+      error: function (data, status, xhr) {
+        alert("获取验证码失败，请稍后再试")
+      }
+    });
+  })
+};
+
+window.onload = function() {
+/**
+ * @description prev page
+ */
+$(".common-header-left").on("click", function () {
+  window.history.go(-1);
+});
+
+/**
+ * @description checkbox
+ */
+$(".common-checkbox").on("click", function () {
+  var t = $(this);
+  var target = t.data("target") ? $("." + t.data("target")) : '';
+  var checked = t.hasClass("checked");
+  var src = t.find("img").attr("src"),dsrc;
+  if(checked){
+    dsrc = src.replace("checkbox1","checkbox")
+  }else {
+    dsrc = src.replace("checkbox","checkbox1")
+  }
+  t.toggleClass("checked").find("img").attr("src", dsrc);
+  // if the user license
+  if (target) target.toggleClass("common-btn-disabled").toggleClass(target.data("class"));
+});
+
+
+/**
+ * @description the  custom radio
+ */
+$(".common-radio").on("tap", function () {
+  $(this).toggleClass("selected")
+});
+
+
+/**
+ * @description the custom tabs
+ */
+$(".common-tab").on("tap", "li", function () {
+  $(this).addClass("active").siblings().removeClass("active")
+});
+
+
+/** todo
+ * @description input focus hide the error tip
+ */
+$(".common-input-item input").on("focus" ,function(){
+  $(this).removeClass("common-input-bottom-error").siblings(".common-error-text").hide();
+  $(this).parent().removeClass("common-input-bottom-error").next(".common-error-text").hide();
+});
+
+}
+
+
---
+++ web/mobile/js/reg.js
@@ -0,0 +1,82 @@
+$(function () {
+  /**
+   * @dec get the vcode
+   */
+  window.GETCODE({
+    btn: $("#sendCode"),
+    input: $("#cell"),
+    api:'/sendSms',
+    country: $("#countriesId")
+  });
+
+
+  /**
+   * @desc submit the form
+   */
+  $(".submit").on("click", function () {
+    if ($(this).hasClass("common-btn-disabled")) return false;
+    var _err_dom = "<div class='common-error-text'> </div>";
+
+    // show error inner func
+    function err(dom,text,isParent){
+      var ele = isParent ? dom.parent() : dom;
+      ele.addClass("common-input-bottom-error").
+      next(".common-error-text").text(text).show();
+
+    }
+
+    // define args
+    var pass = true;
+
+    var regxCode = /^\d{6}$/;
+
+    var regxCell = /^\d{8,}$/; // todo
+
+    var doms = [
+      $("#user"), $("#cell"), $("#code"), $("#pass"), $("#repass")
+    ];
+
+    var tips = [
+      "用户名不可为空",'手机号不可为空',"验证码不可为空","密码不可为空","请再次输入密码"
+    ];
+
+    // check if empty
+    doms.forEach(function(dom,index){
+      if(!dom.val()){
+        err(dom,tips[index],dom.parent().hasClass("common-input-item-label"));
+        pass = false;
+      }else {
+        if(index === 1){
+          if(!regxCell.test(dom.val())){
+            err(dom,"请输入正确的手机号",1);
+          }
+        }
+        if(index === 2){
+          if(!regxCode.test(dom.val())){
+            err(dom,"验证码为6位数字",1);
+          }
+        }
+        if(index >= 3){
+          if(dom.val().length < 6 || dom.val().length > 20){
+            err(dom,"密码长度为6-20之间");
+            pass = false;
+          }else {
+            if(index === 4){
+              if(doms[3].val() !== dom.val()) {
+                err(dom,"两次输入密码不一致");
+                pass = false;
+              }
+            }
+          }
+        }
+      }
+    });
+    // submit the form
+    if(pass) {
+      console.log("pass")
+      $("form").submit();
+    }
+  });
+
+
+});
